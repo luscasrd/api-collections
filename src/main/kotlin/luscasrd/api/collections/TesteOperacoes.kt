ade42e90bae329e6eed09603cb20a4a65838d834
luscasrd <lucas94cr@gmail.com>
2021-03-09 15:39:34 -0300

    created operacoes test class

---
+++ src/main/kotlin/luscasrd/api/collections/TesteOperacoes.kt
@@ -0,0 +1,21 @@
+package luscasrd.api.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios){
+        println(salario)
+    }
+
+    println("-------------")
+
+    println("Maior salário é: ${salarios.max()}")
+    println("Menor salário é: ${salarios.min()}")
+    println("Maior salário é: ${salarios.average()}")
+
+    println("-------------")
+
+    val salariosMaiorQue2500 = salarios.filter{ it > 2500.0 }
+
+    salariosMaiorQue2500.forEach {(println(it))}
+}
