836c7fad2738626f069aabeb045204d3c534a865
luscasrd <lucas94cr@gmail.com>
2021-03-09 14:12:07 -0300

    created in arrayOf test class

---
+++ src/main/kotlin/luscasrd/api/collections/TesteIntArrayOf.kt
@@ -0,0 +1,16 @@
+package luscasrd.api.collections
+
+fun main() {
+    val values = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("----------")
+
+    values.sort()
+    values.forEach { values ->
+        println(values)
+    }
+}
